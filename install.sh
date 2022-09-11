9ff2e9f8adf7b12cb5f3565801944bdf83baf1c3
thamudi <tamim.hamoudi@gmail.com>
2022-09-11 16:21:27 +0300

    feat: update install.sh
    add zsh scripts dir

--- install.sh
+++ install.sh
@@ -6,30 +6,35 @@ echo "====================================="
 
 read -p "Do you want to install dependencies (y/n)? " answer
 case ${answer:0:1} in
-    y|Y )
-        echo 'Installing dependencies using yay package manager'
-				yay -S $(grep -v '^#' arch-pkglist.txt | xargs)
-    ;;
-    * )
-        echo 'Skipping ...'
-    ;;
+y | Y)
+  echo 'Installing dependencies using yay package manager'
+  yay -S $(grep -v '^#' arch-pkglist.txt | xargs)
+  ;;
+*)
+  echo 'Skipping ...'
+  ;;
 esac
 
 echo "====================================="
 echo 'Moving the dot files into the main directory'
 echo "====================================="
 
-cp -r ./dotfiles/{.,} ~ 
+cp -r ./dotfiles/{.,} ~
+
+echo "====================================="
+echo 'Moving the scripts files into the zsh dir'
+echo "====================================="
+
+mkdir -p ~/.zsh/scripts
+
+cp -r ./scripts/{.,} ~/.zsh/scripts
 
 source ~/.zshrc # source the zshrce files
 
-# git the vim plug installed 
+# git the vim plug installed
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-echo 'PlugInstall | vim' # install the vim plugins 
-
-
+echo 'PlugInstall | vim' # install the vim plugins
 
 echo 'Done!'
