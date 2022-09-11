b974d9755c2dbe9787414347293365afc108e75d
thamudi <tamim.hamoudi@gmail.com>
2022-09-11 16:21:53 +0300

    refactor: replace full path with $HOME

--- scripts/garfield.sh
+++ scripts/garfield.sh
@@ -1,5 +1,5 @@
 echo
-lolcat /home/thamudi/.zsh/scripts/garfield.txt
+lolcat $HOME/.zsh/scripts/garfield.txt
 echo
 # echo -e "Hello jerk\b\b\b\b$USER" | pv -qL $[10+(-2 + 5)] | lolcat
 echo "Hello $USER" | lolcat
--- scripts/snoopy.sh
+++ scripts/snoopy.sh
@@ -1,5 +1,5 @@
 echo
-lolcat /home/thamudi/.zsh/scripts/snoopy.txt
+lolcat $HOME/.zsh/scripts/snoopy.txt
 echo
 # echo -e "Hello sleepy\b\b\b\b\b\b$USER" | pv -qL $[10+(-2 + 5)] | lolcat
 echo "Evening $USER" | lolcat
