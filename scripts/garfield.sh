ecf0221eebe4796c97bcf69360b38975f288155f
thamudi <tamim.hamoudi@gmail.com>
2022-09-11 15:06:19 +0300

    refactor: add welcoming scripts
    \

---
+++ scripts/garfield.sh
@@ -0,0 +1,6 @@
+echo
+lolcat /home/thamudi/.zsh/scripts/garfield.txt
+echo
+# echo -e "Hello jerk\b\b\b\b$USER" | pv -qL $[10+(-2 + 5)] | lolcat
+echo "Hello $USER" | lolcat
+echo
---
+++ scripts/snoopy.sh
@@ -0,0 +1,6 @@
+echo
+lolcat /home/thamudi/.zsh/scripts/snoopy.txt
+echo
+# echo -e "Hello sleepy\b\b\b\b\b\b$USER" | pv -qL $[10+(-2 + 5)] | lolcat
+echo "Evening $USER" | lolcat
+echo
